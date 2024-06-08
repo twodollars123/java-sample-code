d6c5652d254b3c7450e9150ddf065f7a3be2b9af
twodollars123 <twodollars.no2002@gmail.com>
2024-06-08 23:34:19 +0700

    06082024-validateData

--- src/main/java/vn/threecents/java_sample_code/controller/UserController.java
+++ src/main/java/vn/threecents/java_sample_code/controller/UserController.java
@@ -1,21 +1,24 @@
 package vn.threecents.java_sample_code.controller;
 
+import jakarta.validation.Valid;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import vn.threecents.java_sample_code.dto.request.UserRequestDTO;
 
 @RestController
 @RequestMapping("/user")
+@Validated
 public class UserController {
     @PostMapping("/")
-    public String addUser(@RequestBody UserRequestDTO user) {
+    public String addUser(@Valid @RequestBody UserRequestDTO user) {
         return "add an user successfully";
     }
     @PutMapping("/{userId}")
-    public String updateUser(@PathVariable int userId, @RequestBody UserRequestDTO user) {
+    public String updateUser(@PathVariable int userId,@Valid @RequestBody UserRequestDTO user) {
         return "updated user successfully";
     }
     @PatchMapping("/{userId}")
-    public String changeUserStatus(@PathVariable int userId, @RequestParam boolean status) {
+    public String changeUserStatus(@PathVariable int userId,@Valid @RequestParam(defaultValue = "0", required = true) boolean status) {
         return "changed user's status successfully";
     }
     @DeleteMapping("/{userId}")
--- src/main/java/vn/threecents/java_sample_code/dto/request/UserRequestDTO.java
+++ src/main/java/vn/threecents/java_sample_code/dto/request/UserRequestDTO.java
@@ -1,18 +1,114 @@
 package vn.threecents.java_sample_code.dto.request;
 
+import jakarta.validation.constraints.*;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import org.springframework.format.annotation.DateTimeFormat;
+import vn.threecents.java_sample_code.util.validatorCustom.PhoneNumber;
+
 import java.io.Serializable;
+import java.util.Date;
+import java.util.Set;
 
 public class UserRequestDTO implements Serializable {
+    @NotBlank(message = "Firstname must be not black")
     private String firstName;
+    @NotNull(message = "Lastname must be not null")
     private String lastName;
+    @Email(message = "Email invalid format")
     private String email;
+//    @Pattern(regexp = "^\\d{10}$", message = "Phone invalid format")
+    @PhoneNumber(message = "Phone invalid format")
     private String phone;
-
+    @NotNull(message = "DateOfBirth must be not null")
+    @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @JsonFormat(pattern = "MM/dd/yyyy")
+    private Date dateOfBirth;
+    @NotNull(message = "Username must be not null")
+    private String userName;
+    @NotNull(message = "Password must be not null")
+    private String password;
+    @NotEmpty(message = "Addresses must be not empty")
+    private Set<Address> addresses;
     public UserRequestDTO(String firstName, String lastName, String email, String phone) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.phone = phone;
+    }
+
+    public static class Address {
+        private String apartmentNumber;
+        private String floor;
+        private String building;
+        private String streetNumber;
+        private String street;
+        private String city;
+        private String country;
+        private Integer addressType;
+
+        public String getApartmentNumber() {
+            return apartmentNumber;
+        }
+
+        public void setApartmentNumber(String apartmentNumber) {
+            this.apartmentNumber = apartmentNumber;
+        }
+
+        public String getFloor() {
+            return floor;
+        }
+
+        public void setFloor(String floor) {
+            this.floor = floor;
+        }
+
+        public String getBuilding() {
+            return building;
+        }
+
+        public void setBuilding(String building) {
+            this.building = building;
+        }
+
+        public String getStreetNumber() {
+            return streetNumber;
+        }
+
+        public void setStreetNumber(String streetNumber) {
+            this.streetNumber = streetNumber;
+        }
+
+        public String getStreet() {
+            return street;
+        }
+
+        public void setStreet(String street) {
+            this.street = street;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public void setCity(String city) {
+            this.city = city;
+        }
+
+        public String getCountry() {
+            return country;
+        }
+
+        public void setCountry(String country) {
+            this.country = country;
+        }
+
+        public Integer getAddressType() {
+            return addressType;
+        }
+
+        public void setAddressType(Integer addressType) {
+            this.addressType = addressType;
+        }
     }
 
     public String getFirstName() {
@@ -45,5 +141,37 @@ public class UserRequestDTO implements Serializable {
 
     public void setPhone(String phone) {
         this.phone = phone;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(Set<Address> addresses) {
+        this.addresses = addresses;
     }
 }
